c57c472908cad2ce2c7358f710cba2a8ed06e71d
CTepedino <ctepedino@itba.edu.ar>
2025-06-06 21:04:17 -0300

    Distribute and recover with metadata for k!=8

--- secret-image-sharing/src/main/java/ar/edu/itba/cripto/LSB.java
+++ secret-image-sharing/src/main/java/ar/edu/itba/cripto/LSB.java
@@ -6,7 +6,6 @@ import java.util.Random;
 
 public class LSB {
 
-
     public static byte[][] distribute(byte[][] image, byte[] shadow) {
         int height = image.length;
         int width = image[0].length;
@@ -68,34 +67,127 @@ public class LSB {
         return result;
     }
 
+    public static byte[][] distributeWithMetadata(byte[][] image, byte[] shadow, int secretHeight, int secretWidth){
+        int rows = image.length;
+        int cols = image[0].length;
+
+        byte[] meta = new byte[4];
+        meta[0] = (byte) ((secretHeight >> 8) & 0xFF);
+        meta[1] = (byte) (secretHeight & 0xFF);
+        meta[2] = (byte) ((secretWidth >> 8) & 0xFF);
+        meta[3] = (byte) (secretWidth & 0xFF);
+
+        int totalBits = 32 + shadow.length * 8;
+
+        byte[][] result = new byte[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            result[i] = image[i].clone();
+        }
+
+        int bitIndex = 0;
+
+        for (int i = 0; i < 4; i++) {
+            for (int b = 7; b >= 0; b--) {
+                int bit = (meta[i] >> b) & 1;
+                int row = bitIndex / cols;
+                int col = bitIndex % cols;
+                result[row][col] = (byte) ((result[row][col] & 0xFE) | bit);
+                bitIndex++;
+            }
+        }
+
+        for (byte value : shadow) {
+            for (int b = 7; b >= 0; b--) {
+                int bit = (value >> b) & 1;
+                int row = bitIndex / cols;
+                int col = bitIndex % cols;
+                result[row][col] = (byte) ((result[row][col] & 0xFE) | bit);
+                bitIndex++;
+            }
+        }
+
+        return result;
+    }
+
+    public static class RecoveryResult {
+        public final byte[] shadow;
+        public final int secretHeight;
+        public final int secretWidth;
+
+        public RecoveryResult(byte[] shadow, int secretHeight, int secretWidth) {
+            this.shadow = shadow;
+            this.secretHeight = secretHeight;
+            this.secretWidth = secretWidth;
+        }
+    }
+
+    public static RecoveryResult recoverWithMetadata(byte[][] image, int k){
+        int rows = image.length;
+        int cols = image[0].length;
+
+        int bitIndex = 0;
+
+        int secretHeight = 0, secretWidth = 0;
+        for (int i = 0; i < 16; i++) {
+            int row = bitIndex / cols;
+            int col = bitIndex % cols;
+            int bit = image[row][col] & 1;
+            secretHeight = (secretHeight << 1) | bit;
+            bitIndex++;
+        }
+        for (int i = 0; i < 16; i++) {
+            int row = bitIndex / cols;
+            int col = bitIndex % cols;
+            int bit = image[row][col] & 1;
+            secretWidth = (secretWidth << 1) | bit;
+            bitIndex++;
+        }
+
+        int shadowSize = (secretHeight * secretWidth) / k;
+        byte[] shadow = new byte[shadowSize];
+
+        for (int i = 0; i < shadowSize; i++) {
+            int value = 0;
+            for (int b = 0; b < 8; b++) {
+                int row = bitIndex / cols;
+                int col = bitIndex % cols;
+                int bit = image[row][col] & 1;
+                value = (value << 1) | bit;
+                bitIndex++;
+            }
+            shadow[i] = (byte) value;
+        }
+
+        return new RecoveryResult(shadow, secretHeight, secretWidth);
+    }
 
     public static void main(String[] args) throws IOException {
 
         Random random = new Random(10);
 
-        BMP test = new BMP("target.bmp");
+        BMP test = new BMP("result.bmp");
 
-        byte[] secret = new byte[(test.width*test.height)/8];
-        for(int i = 0; i < secret.length; i++){
-            secret[i] = (byte) random.nextInt(256);
+        byte[] shadow = new byte[(100*101)/8];
+        for(int i = 0; i < shadow.length; i++){
+            shadow[i] = (byte) random.nextInt(256);
         }
 
-        byte[][] distributed = distribute(test.pixels, secret);
+        byte[][] distributed = distributeWithMetadata(test.pixels, shadow, 100, 101);
 
         new BMP(distributed).toFile("distributed.bmp");
 
-        byte[] recovered = recover(distributed);
+        RecoveryResult recovered = recoverWithMetadata(distributed, 8);
 
 
-        System.out.println(Arrays.toString(recovered));
+        System.out.println(recovered.secretHeight);
+        System.out.println(recovered.secretWidth);
 
         random = new Random(10);
-        for(int i  = 0; i < secret.length; i++){
-            if (recovered[i] != (byte) random.nextInt(256)){
+        for(int i  = 0; i < shadow.length; i++){
+            if (recovered.shadow[i] != (byte) random.nextInt(256)){
                 throw new RuntimeException("bad");
             }
         }
-
 
     }
 }
